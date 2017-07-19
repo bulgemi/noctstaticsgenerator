91bbb54ce47a93575013fae8a1a10ff58ff0fde8
kim dong-hun <donghun_kim@sk.com>
2017-07-19 14:34:41 +0900

    상세 결과 추가.

--- noctstaticsgenerator3.py
+++ noctstaticsgenerator3.py
@@ -1,5 +1,7 @@
 # _*_ coding: utf-8 _*_
-
+"""
+    특정 기간동안 처리한 건수를 이용하여 일별 차수별 통계 데이터를 엑셀 포맷으로 생성하는 도구.
+"""
 import xlsxwriter
 import re
 
@@ -25,8 +27,13 @@ def create_excel():
     header_format.set_pattern(1)
     header_format.set_bg_color('gray')
 
+    date_format = workbook.add_format({'bold': False, 'align': 'left', 'border': 1})
+
     number_format = workbook.add_format({'border': 1})
     number_format.set_num_format('#,##0')
+
+    number_format2 = workbook.add_format({'bold': True, 'border': 1})
+    number_format2.set_num_format('#,##0')
 
     max_number_format = workbook.add_format({'bold': True, 'border': 1})
     max_number_format.set_num_format('#,##0')
@@ -100,16 +107,18 @@ def create_excel():
     worksheet_sum.insert_chart('A7', chart)
 
     column_index += 1
-    worksheet_sum.write(1, column_index, total_sum, number_format)
+    worksheet_sum.write(1, column_index, total_sum, number_format2)
     column_index += 1
-    worksheet_sum.write(1, column_index, total_avg, number_format)
+    worksheet_sum.write(1, column_index, total_avg, number_format2)
 
+    # 항목별 목차 생성.
     # 통계 상세 타이틀
     column_index = 0
     worksheet.write_string(0, column_index, u'일자', header_format)
     worksheet.set_column(0, column_index, 20)
     cycleinfolist = data[0]
 
+    # 차수 컬럼 생성.
     for cycleinfo in cycleinfolist:
         cycleinfo_yn = re.findall(',', cycleinfo)
 
@@ -119,9 +128,76 @@ def create_excel():
         column_index += 1
 
         cycle_time_list = re.split(',', cycleinfo)
-        cycle_time = cycle_time_list[0] + "(" + cycle_time_list[1] + ")"
+        cycle_time = cycle_time_list[0] + u" 차수(" + cycle_time_list[1] + ")"
         worksheet.write_string(0, column_index, cycle_time, header_format)
         worksheet.set_column(0, column_index, 20)
+
+    # 일별 총합 컬럼 생성.
+    column_index += 1
+    worksheet.write_string(0, column_index, u"일별 합계", header_format)
+    worksheet.set_column(0, column_index, 20)
+
+    # 차수 평균 컬럼 생성.
+    column_index += 1
+    worksheet.write_string(0, column_index, u"차수 평균", header_format)
+    worksheet.set_column(0, column_index, 20)
+
+    # 일자별 처리 데이터 생성.
+    row_index = 1 # row 인덱스 초기화.
+
+    # 차수별 처리건수 합계 리스트 생성.
+    cyclesum = [0 for _ in range(48)]
+#    print(cyclesum)
+
+    for v in data:
+        if len(v) < 50:
+            break
+
+        daysum = 0
+        column_index = 0 # 컬럼 인덱스 초기화.
+
+        for c in v:
+            if column_index == 49:
+                worksheet.write(row_index, column_index, daysum, number_format2)
+                worksheet.write(row_index, column_index+1, daysum/48, number_format2)
+
+            if len(c) <= 1:
+                break
+
+            cycledata_yn = re.findall(',', c)
+
+            if len(cycledata_yn) == 0:
+                # 일자 컬럼 생성.
+                worksheet.write_string(row_index, column_index, c, date_format)
+            else:
+                # 차수별 처리 건수 생성.
+                cycledata = re.split(',', c)
+                daysum += int(cycledata[2])
+                cyclesum[column_index-1] += int(cycledata[2])
+                worksheet.write(row_index, column_index, int(cycledata[2]), number_format)
+
+            column_index += 1 # 컬럼 인덱스 증가.
+
+
+        row_index += 1 # row 인덱스 증가.
+
+    # 최대 처리 건수 차수 추출.
+
+    # 차수 평균 컬럼 생성.
+    column_index = 0
+    worksheet.write_string(row_index, column_index, u"차수 합계", header_format)
+
+    total_sum = 0
+
+    for c in cyclesum:
+        column_index += 1
+        worksheet.write(row_index, column_index, c, number_format2)
+        total_sum += c
+
+    column_index += 1
+    worksheet.write_string(row_index, column_index, u"총합", header_format)
+    column_index += 1
+    worksheet.write(row_index, column_index, total_sum, number_format2)
 
     workbook.close()
 
